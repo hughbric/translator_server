ecf5071fc29d4183eb6f58c028d7feeba0fe4241
Michael Hughes <mhughes36@live.co.uk>
2019-08-30 12:05:49 +0100

    translates vowels and multiple words

--- app/models/term.rb
+++ app/models/term.rb
@@ -4,10 +4,41 @@ class Term < ApplicationRecord
   VOWELS = ['A', 'E', 'I', 'O', 'U']
 
   def translate_term(term)
-    if !VOWELS.include?(term[0].upcase)
-      first_letter = term.slice(0)
-      last_letters = term[1..-1]
-      output = last_letters + first_letter + 'ay'
+    return if term == nil
+    output = ''
+    words = term.split
+
+    words.each do |word|
+      if !output.empty?
+        output << ' '
+      end
+
+      if !VOWELS.include?(word[0].upcase)
+        if !VOWELS.include?(word[1].upcase)
+          output << mutate_double_consonant(word)
+        else
+          output << mutate_consonant(word)
+        end
+      end
+
+      if VOWELS.include?(word[0].upcase)
+        output <<  word + 'way'
+      end
     end
+    output
+  end
+
+  private
+
+  def mutate_double_consonant(word)
+    first_letters = word.slice(0..1)
+    last_letters = word[2..-1]
+    output = last_letters + first_letters + 'ay'
+  end
+
+  def mutate_consonant(word)
+    first_letter = word.slice(0)
+    last_letters = word[1..-1]
+    output = last_letters + first_letter + 'ay'
   end
 end
--- spec/models/term_spec.rb
+++ spec/models/term_spec.rb
@@ -6,5 +6,25 @@ RSpec.describe Term, type: :model do
       term = Term.new
       expect(term.translate_term('pig')).to eq('igpay')
     end
+
+    it 'staring with vowels' do
+      term = Term.new
+      expect(term.translate_term('igloo')).to eq('iglooway')
+    end
+
+    it 'single word vowels' do
+      term = Term.new
+      expect(term.translate_term('I')).to eq('Iway')
+    end
+
+    it 'consonant clusters' do
+      term = Term.new
+      expect(term.translate_term('glove')).to eq('oveglay')
+    end
+
+    it 'translates multiple words' do
+      term = Term.new
+      expect(term.translate_term('good evening')).to eq('oodgay eveningway')
+    end
   end
 end
