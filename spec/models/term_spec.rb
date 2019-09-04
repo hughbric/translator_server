7f10597cdf7c90e513939784ad004d5e5a058d76
Michael Hughes <mhughes36@live.co.uk>
2019-09-04 23:07:15 +0100

    refactor

--- spec/models/term_spec.rb
+++ spec/models/term_spec.rb
@@ -3,28 +3,23 @@ require 'rails_helper'
 RSpec.describe Term, type: :model do
   describe 'translate_term' do
     it 'starting with consonants' do
-      term = Term.new
-      expect(term.translate_term('pig')).to eq('igpay')
+      expect(subject.translate_term('pig')).to eq('igpay')
     end
 
     it 'staring with vowels' do
-      term = Term.new
-      expect(term.translate_term('igloo')).to eq('iglooway')
+      expect(subject.translate_term('igloo')).to eq('iglooway')
     end
 
     it 'single word vowels' do
-      term = Term.new
-      expect(term.translate_term('I')).to eq('Iway')
+      expect(subject.translate_term('I')).to eq('Iway')
     end
 
     it 'consonant clusters' do
-      term = Term.new
-      expect(term.translate_term('glove')).to eq('oveglay')
+      expect(subject.translate_term('glove')).to eq('oveglay')
     end
 
     it 'translates multiple words' do
-      term = Term.new
-      expect(term.translate_term('good evening')).to eq('oodgay eveningway')
+      expect(subject.translate_term('good evening')).to eq('oodgay eveningway')
     end
   end
 end
