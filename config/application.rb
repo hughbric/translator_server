87dcb7068444c381c30da1d6a22dd84d99466f6c
Michael Hughes <mhughes36@live.co.uk>
2019-09-08 01:29:12 +0100

    rack config

--- config/application.rb
+++ config/application.rb
@@ -22,11 +22,11 @@ Bundler.require(*Rails.groups)
 module TranslatorServer
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    # config.load_defaults 6.0
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-         origins '*'
+         origins 'https://vigorous-blackwell-e2069e.netlify.com/'
          resource '*', :headers => :any, :methods => [:get, :post, :options]
        end
     end
